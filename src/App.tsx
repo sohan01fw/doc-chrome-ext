baba0e9d9ab307c2ed94d463e4a191acd2adb04e
sohan01fw <sohancwactfw@gmail.com>
2024-11-26 20:13:36 +0545

    implement sidepanel for extention

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import "./App.css";
-
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="w-[300px] h-[200px] bg-gray-100 p-4 rounded-lg shadow-lg">
-      <h1 className="text-red-500 text-lg font-bold">
-        Hey, this is me using Vim mode in Zed Editor!
-      </h1>
-      <button
-        onClick={() => alert("Welcome here")}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-      >
-        Click Me
-      </button>
+    <div>
+      <h1> hey </h1>
     </div>
   );
-}
+};
 
 export default App;
